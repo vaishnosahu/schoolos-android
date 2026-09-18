df36ff4b354e2f734c934dccb190a456ece4b8de
Vaishno Sahu <vaishno.sahu@gmail.com>
2026-09-18 10:08:35 +0530

    Native Mobile 3.0.12 password toggle cache refresh

--- app/src/main/java/com/schooloss/mobile/MainActivity.java
+++ app/src/main/java/com/schooloss/mobile/MainActivity.java
@@ -60,9 +60,9 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class MainActivity extends Activity {
-    private static final String NATIVE_VERSION = "3.0.11";
-    private static final String HOME = "https://alkeynesprjects.com/schools/mobile/?native_app=android&native_version=3.0.11";
-    private static final String APP_UA = " SchoolOSNative/3.0.11 Android";
+    private static final String NATIVE_VERSION = "3.0.12";
+    private static final String HOME = "https://alkeynesprjects.com/schools/mobile/?native_app=android&native_version=3.0.12";
+    private static final String APP_UA = " SchoolOSNative/3.0.12 Android";
     private static final int FILE_REQ = 4101;
     private static final int WEB_PERM_REQ = 4102;
     private static final int GEO_PERM_REQ = 4103;

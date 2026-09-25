4c1d57f499a0e994925daa04fb242ed64d04b26a
Vaishno Sahu <vaishno.sahu@gmail.com>
2026-09-25 19:44:27 +0530

    Add full native modular core

---
+++ app/src/main/java/com/schooloss/fullnative/core/Constants.java
@@ -0,0 +1,6 @@
+package com.schooloss.fullnative.core;
+public final class Constants {
+ public static final String API="https://alkeynesprjects.com/schools/mobile-api/v1/index.php";
+ public static final int BLUE=0xFF3657D6, BG=0xFFF7F9FD, TEXT=0xFF192132, MUTED=0xFF6A748C, LINE=0xFFE5E9F2;
+ private Constants(){}
+}
